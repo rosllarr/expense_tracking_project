03e036661c1e9fc694f41a1f9c9e6e63a60881ea
me <rosllarr@tutanota.com>
2023-06-04 06:39:02 +0700

    add Transection model

--- expensestracking/core/models.py
+++ expensestracking/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Transection(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    date = models.DateField(blank=True)
+    due_date = models.DateField(blank=True)
+
+    class Meta:
+        abstract = True
