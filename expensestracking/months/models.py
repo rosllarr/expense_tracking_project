199d4b194977a9a1ea98add1e622781e066684a8
me <rosllarr@tutanota.com>
2023-06-04 06:57:50 +0700

    add Month, Catalog models

--- expensestracking/months/models.py
+++ expensestracking/months/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from core.models import Transection
+
+
+class Month(Transection):
+    income = models.BooleanField()
+    expenses = models.BooleanField()
+
+
+class Catalog(models.Model):
+    entry = models.ForeignKey(Month, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
