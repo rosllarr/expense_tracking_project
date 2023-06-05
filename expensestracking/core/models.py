5c96d5a0386c55c53458d65eaff2287a909da4a4
me <rosllarr@tutanota.com>
2023-06-05 21:18:00 +0700

    allow fields date and due_date to be null

--- expensestracking/core/models.py
+++ expensestracking/core/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Transection(models.Model):
     name = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=9, decimal_places=2)
-    date = models.DateField(blank=True)
-    due_date = models.DateField(blank=True)
+    date = models.DateField(null=True)
+    due_date = models.DateField(null=True)
 
     class Meta:
         abstract = True
